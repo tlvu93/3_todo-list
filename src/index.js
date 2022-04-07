4ef2cee4c2beba8a01e6429208e370daa5a30030
“tlvu93” <101603072+tlvu93@users.noreply.github.com>
2022-04-07 17:14:09 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 const defaultTasks = ["Homework", "Shopping", "Learning CS", "Create a todo list"]
 
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    let taskList = document.getElementById("task-list")
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     let taskList = document.getElementById("task-list")
 
-    defaultTasks.forEach(task => {
+//     defaultTasks.forEach(task => {
 
 
-        const listItem = document.createElement("div")
-        listItem.innerHTML = `<li class="list-group-item">
-        <div class="form-check d-flex align-items-center justify-content-between">
-          <div>
-            <input
-              class="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
+//         const listItem = document.createElement("div")
+//         listItem.innerHTML = `<li class="list-group-item">
+//         <div class="form-check d-flex align-items-center justify-content-between">
+//           <div>
+//             <input
+//               class="form-check-input"
+//               type="checkbox"
+//               value=""
+//               id="flexCheckDefault"
+//             />
       
-            <label class="form-check-label" for="flexCheckDefault"> ${task} </label>
-          </div>
-          <div class="button-group">
-            <button type="button" class="btn btn-warning">
-              <i class="fa-solid fa-pen"></i>
-            </button>
-            <button type="button" class="btn btn-danger px-3">
-              <i class="fa-solid fa-trash-can"></i>
-            </button>
-          </div>
-        </div>
-      </li>`
+//             <label class="form-check-label" for="flexCheckDefault"> ${task} </label>
+//           </div>
+//           <div class="button-group">
+//             <button type="button" class="btn btn-warning">
+//               <i class="fa-solid fa-pen"></i>
+//             </button>
+//             <button type="button" class="btn btn-danger px-3">
+//               <i class="fa-solid fa-trash-can"></i>
+//             </button>
+//           </div>
+//         </div>
+//       </li>`
 
-        taskList.appendChild(listItem)
-    })
-});
+//         taskList.appendChild(listItem)
+//     })
+// });
